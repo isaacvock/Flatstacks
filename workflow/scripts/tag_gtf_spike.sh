d84c848b1d3a88de7ecc1164ba2316795bcc1dc0
isaacvock <isaac.vock@gmail.com>
2023-07-07 13:36:46 -0400

    Proper handling of bash varible in sed

--- workflow/scripts/tag_gtf_spike.sh
+++ workflow/scripts/tag_gtf_spike.sh
@@ -6,4 +6,4 @@ tag=$4
 awk -v OFS="\t" -v FS="\t" -v addedtag="$tag" ' $1 !~ /^#/ {$1 = "chr"$1"_"addedtag}
         {print $0}' "$input" > "$output_temp"
 
-sed -r 's/(gene_name ")([^"]*)/\1\2_"$tag"/g' "$output_temp" > "$output"
+sed -r 's/(gene_name ")([^"]*)/\1\2_$tag/g' "$output_temp" > "$output"
